d4ffc67df087a7d4eee798e672e316e0b765c85e
Enrique Largacha Gil <enrique.largacha@outlook.com>
2022-07-01 11:15:47 +0200

    Added app screens

---
+++ src/features/heroes/screens/HeroesDetailScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet, Image } from "react-native";
+
+const HeroesDetailScreen = () => {
+
+    return (
+        <View>
+            <Text>Detail Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create ({});
+
+export default HeroesDetailScreen;
---
+++ src/features/heroes/screens/HeroesListScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet, Button, FlatList, Image } from "react-native";
+
+const HeroesListScreen = () => {
+
+    return (
+        <View>
+            <Text>List Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create ({});
+
+export default HeroesListScreen;
